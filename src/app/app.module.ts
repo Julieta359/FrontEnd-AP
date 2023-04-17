a21bb17ae02cdb8b1d0d1a739a47cefbd2bf3edf
Julieta359 <119441514+Julieta359@users.noreply.github.com>
2023-04-17 19:47:19 -0300

    URL render

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,9 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     HomeComponent,
     LoginComponent,
     NewExperienciaComponent,
-    EditExperienciaComponent
+    EditExperienciaComponent,
+    EditeducacionComponent,
+    NeweducacionComponent
   ],
   imports: [
     BrowserModule,
